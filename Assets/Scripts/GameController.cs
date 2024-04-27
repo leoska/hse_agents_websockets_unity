a67116842d31bb06b595b91c80fe788cdee8cace
leoska <leo77551@gmail.com>
2024-04-27 20:19:23 +0300

    27 April lession

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -12,12 +13,15 @@ public class GameController : MonoBehaviour
 
     public int _tickRate = 60;
     public float _speed = 2.0f;
+    public float lagTimer = 0f;
     
     private WebSocket _socket;
     private GameObject _current;
     
     private float _timer = 0f;
     private float _timerLimit = 0f;
+
+    public bool _lag = false;
     
     // Start is called before the first frame update
     async void Start()
@@ -25,6 +29,8 @@ public class GameController : MonoBehaviour
         _timerLimit = 1f / _tickRate;
         
         _socket = new WebSocket("ws://127.0.0.1:8080/ws");
+
+        _lag = false;
 
         _socket.OnOpen += () =>
         {
@@ -43,47 +49,56 @@ public class GameController : MonoBehaviour
         
         _socket.OnMessage += (bytes) =>
         {
-            var message = System.Text.Encoding.UTF8.GetString(bytes);
-
-            var packet = JsonUtility.FromJson<Packet>(message);
-
-            switch (packet.event_type)
+            StartCoroutine(StartWait(lagTimer, () =>
             {
-                case 1:
-                    if (packet.mode == "red")
-                    {
-                        _current = leftPlayer;
-                    } 
-                    else if (packet.mode == "blue")
-                    {
-                        _current = rightPlayer;
-                    }
-                    break;
+                var message = System.Text.Encoding.UTF8.GetString(bytes);
+
+                var packet = JsonUtility.FromJson<Packet>(message);
+
+                switch (packet.event_type)
+                {
+                    case 1:
+                        if (packet.mode == "red")
+                        {
+                            _current = leftPlayer;
+                        } 
+                        else if (packet.mode == "blue")
+                        {
+                            _current = rightPlayer;
+                        }
+                        break;
                 
-                case 10:
-                    float position_red = packet.position_red;
-                    float position_blue = packet.position_blue;
+                    case 10:
+                        float position_red = packet.position_red;
+                        float position_blue = packet.position_blue;
 
-                    if (_current != leftPlayer)
-                    {
-                        leftPlayer.transform.position = new Vector3(-7f, position_red, 0f);
-                    }
+                        if (_current != leftPlayer)
+                        {
+                            leftPlayer.transform.position = new Vector3(-7f, position_red, 0f);
+                        }
 
-                    if (_current != rightPlayer)
-                    {
-                        rightPlayer.transform.position = new Vector3(7f, position_blue, 0f);
-                    }
+                        if (_current != rightPlayer)
+                        {
+                            rightPlayer.transform.position = new Vector3(7f, position_blue, 0f);
+                        }
 
-                    ball.transform.position = new Vector3(packet.ball_x, packet.ball_y, 0f);
+                        ball.transform.position = new Vector3(packet.ball_x, packet.ball_y, 0f);
                     
-                    break;
+                        break;
                 
-                default:
-                    throw new Exception($"Undefined event type: {message}");
-            }
+                    default:
+                        throw new Exception($"Undefined event type: {message}");
+                }
+            }));
         };
 
         await _socket.Connect();
+    }
+
+    private IEnumerator StartWait(float time, Action callback)
+    {
+        yield return new WaitForSeconds(time);
+        callback();
     }
 
     void DispatchMessageQueue()
@@ -129,13 +144,18 @@ public class GameController : MonoBehaviour
 
         Move();
 
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            _lag = !_lag;
+        }
+
         if (_current == null)
         {
             return;
         }
 
         _timer += Time.deltaTime;
-        if (_timer < _timerLimit)
+        if (_timer < _timerLimit || _lag)
         {
             return;
         }
